5fde32912e783311b199724af3d114e8a4c495f3
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-08-04 23:58:46 +0530

    Day 3 - Array Methods: Callback syntax, forEach, filter, map, Registration Form Validation: email validation pointers implemented

--- JavaScript/Projects/RegistrationForm/index.js
+++ JavaScript/Projects/RegistrationForm/index.js
@@ -7,11 +7,45 @@ function validate() {
 	let zipInput = document.getElementById('zip').value
 	// let tnCInput = document.getElementById('tnC').checked
 
-	if(firstNameInput.length >= 1){
+	if(firstNameInput){
 		document.getElementById('first-name-valid').style.display = 'block'
 		document.getElementById('first-name-invalid').style.display = 'none'
 	} else {
 		document.getElementById('first-name-invalid').style.display = 'block'
 		document.getElementById('first-name-valid').style.display = 'none'
 	}
+
+	if(lastNameInput){
+		document.getElementById('last-name-valid').style.display = 'block'
+		document.getElementById('last-name-invalid').style.display = 'none'
+	} else {
+		document.getElementById('last-name-invalid').style.display = 'block'
+		document.getElementById('last-name-valid').style.display = 'none'
+	}
+
+	/* Email
+	- Whether exists? - Done
+	- @ - Done
+	- . - Done
+	- After last ., there should be atleast 2 characters - Done
+	- First character cannot be @ - Done
+	*/
+
+	// Eg: ankit@gmail.com
+	// Index of .: 11
+	// Length of string: 15
+
+	if (
+    emailInput &&
+    emailInput.includes("@") &&
+    emailInput.includes(".") &&
+    emailInput.lastIndexOf(".") <= emailInput.length - 3 &&
+		emailInput.indexOf('@') !== 0
+  ) {
+    document.getElementById("email-valid").style.display = "block";
+    document.getElementById("email-invalid").style.display = "none";
+  } else {
+    document.getElementById("email-invalid").style.display = "block";
+    document.getElementById("email-valid").style.display = "none";
+  }
 }
