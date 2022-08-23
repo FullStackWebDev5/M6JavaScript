6b2f646a52592ca623886e4d04063c6b52f7497e
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-08-23 22:45:07 +0530

    Order Status Management V2: Cancel Order UI, Logic, Multiple items UI complete

--- JavaScript/Projects/OrderStatusManagementV2/js/createOrderCard.js
+++ JavaScript/Projects/OrderStatusManagementV2/js/createOrderCard.js
@@ -4,6 +4,7 @@ const createOrderCard = (orderId) => {
 	let colDiv = document.createElement('div')
 	let cardDiv = document.createElement('div')
 	let cardHeader = document.createElement('div')
+	let cancelBtn = document.createElement('i')
 	let cardBody = document.createElement('div')
 	let cardText1 = document.createElement('p')
 	let cardText2 = document.createElement('p')
@@ -14,6 +15,7 @@ const createOrderCard = (orderId) => {
 	colDiv.classList = 'col-md-3 order-status-card'
 	cardDiv.classList = 'card text-center'
 	cardHeader.classList = 'card-header'
+	cancelBtn.classList = 'fa-solid fa-xmark cancel-btn'
 	cardBody.classList = 'card-body'
 	cardText1.classList = 'card-text'
 	cardText2.classList = 'card-text'
@@ -21,7 +23,7 @@ const createOrderCard = (orderId) => {
 	statusBadge.classList = 'badge rounded-pill bg-success'
 	cardFooter.classList = 'card-footer'
 
-	cardHeader.innerHTML = `Order ID: <b>${orderId}</b>`
+	cardHeader.innerHTML = `<span>Order ID: <b>${orderId}</b></span>`
 	cardText1.innerText = 'Medium Size Pizza - 2 Nos'
 	cardText2.innerHTML = 'Bill Amount: <b>$22</b>'
 	statusBadge.innerText ='First layer of cheeze added'
@@ -36,6 +38,7 @@ const createOrderCard = (orderId) => {
 
 	statusBadge.id = orderId
 
+	cardHeader.appendChild(cancelBtn)
 	cardDiv.appendChild(cardHeader)
 	cardBody.appendChild(cardText1)
 	cardBody.appendChild(cardText2)
@@ -45,22 +48,25 @@ const createOrderCard = (orderId) => {
 	cardDiv.appendChild(cardFooter)
 	colDiv.appendChild(cardDiv)
 	orderCardsWrapper.appendChild(colDiv)
+
+	cancelBtn.addEventListener('click', () => {
+		// orderCardsWrapper.removeChild(colDiv)
+		colDiv.style.display = 'none'
+	})
 }
 
-// <div class="col-md-3 order-status-card">
-// 	<div class="card text-center">
-// 		<div class="card-header">
-// 			Order ID: <b>MP202200001</b>
-// 		</div>
-// 		<div class="card-body">
-// 			<p class="card-text">Medium Size Pizza - 2 Nos</p>
-// 			<p class="card-text">Bill Amount: <b>$22</b></p>
-// 			<p class="card-text">
-// 				<span class="badge rounded-pill bg-success">First layer of cheeze added</span>
-// 			</p>
-// 		</div>
-// 		<div class="card-footer">
-// 			18-08-2022 10:24 PM
-// 		</div>
-// 	</div>
-// </div>
+/* <div class="col-md-3 order-status-card">
+	<div class="card text-center">
+		<div class="card-header">
+			<span>Order ID: <b>shbjdkms</b></span>
+			<i class="fa-solid fa-xmark"></i>
+		</div>
+		<div class="card-body">
+			<p class="card-text">Medium Size Pizza - 2 Nos</p>
+			<p class="card-text">Bill Amount: <b>$22</b></p>
+			<p class="card-text"><span class="badge rounded-pill bg-success" id="shbjdkms">Package received at
+					counter</span></p>
+		</div>
+		<div class="card-footer">August 23, 2022 at 09:50:51 PM</div>
+	</div>
+</div> */
