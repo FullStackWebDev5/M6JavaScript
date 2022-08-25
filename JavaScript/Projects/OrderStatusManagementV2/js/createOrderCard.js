3e65e25a7ecfcec2d8e7fee5bacd3f1ba73e3a91
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-08-25 22:54:25 +0530

    Order Status Management V2: Create Bill Table Layout Updated, Dynamic Items Array, Total Bill Logic

--- JavaScript/Projects/OrderStatusManagementV2/js/createOrderCard.js
+++ JavaScript/Projects/OrderStatusManagementV2/js/createOrderCard.js
@@ -6,60 +6,15 @@ const createOrderCard = (orderId) => {
 	let cardHeader = document.createElement('div')
 	let cancelBtn = document.createElement('i')
 	let cardBody = document.createElement('div')
-	let cardText1 = document.createElement('p')
 	let cardText2 = document.createElement('p')
 	let statusBadge = document.createElement('span')
 	let cardFooter = document.createElement('div')
-
-	let billTable = document.createElement('table')
-	let billTableHeader = document.createElement('thead')
-	let billTableRows = []
-
-	let billDetails = [
-    {
-      item: "Medium Corn",
-      price: "$5.00",
-    },
-    {
-      item: "Large Mushroom",
-      price: "$7.50",
-    },
-    {
-      item: "Medium Paneer",
-      price: "$4.75",
-    },
-  ];
-
-	for(let i = 0; i < 5; i++){
-		billTableRows.push(document.createElement('tr'))
-		let billTableColumns = []
-		for(let j = 0; j < 3; j++) {
-			if(i === 0) {
-				billTableColumns.push(document.createElement('th'))
-			} else {
-				billTableColumns.push(document.createElement('td'))
-			}
-		}
-		if(i === 0){
-			billTableColumns[0].innerText = '#'
-			billTableColumns[1].innerText = 'Item'
-			billTableColumns[2].innerText = 'Price'
-		} else if (i != 4) {
-			console.log(i)
-			billTableColumns[0].innerText = i
-			billTableColumns[1].innerText = billDetails[i - 1].item
-			billTableColumns[2].innerText = billDetails[i - 1].price
-		}
-		billTableRows[i].append(...billTableColumns)
-	}
 
 	colDiv.classList = 'col-md-3 order-status-card'
 	cardDiv.classList = 'card text-center'
 	cardHeader.classList = 'card-header'
 	cancelBtn.classList = 'fa-solid fa-xmark cancel-btn'
 	cardBody.classList = 'card-body'
-	billTable.classList = 'table'
-	cardText1.classList = 'card-text'
 	cardText2.classList = 'card-text'
 	statusBadge.classList = 'badge rounded-pill bg-success'
 	cardFooter.classList = 'card-footer'
@@ -77,12 +32,11 @@ const createOrderCard = (orderId) => {
 
 	statusBadge.id = orderId
 
+	let billTable = createOrderCardBillTable()
+
 	cardHeader.appendChild(cancelBtn)
 	cardDiv.appendChild(cardHeader)
-	cardText1.appendChild(billTable)
-	billTableHeader.append(...billTableRows)
-	billTable.appendChild(billTableHeader)
-	cardBody.appendChild(cardText1)
+	cardBody.appendChild(billTable)
 	cardText2.appendChild(statusBadge)
 	cardBody.appendChild(cardText2)
 	cardDiv.appendChild(cardBody)
@@ -96,20 +50,20 @@ const createOrderCard = (orderId) => {
 	})
 }
 
-/* <div class="col-md-3 order-status-card">
+{/* <div class="col-md-3 order-status-card">
 	<div class="card text-center">
-		<div class="card-header">
-			<span>Order ID: <b>shbjdkms</b></span>
-			<i class="fa-solid fa-xmark"></i>
+		<div class="card-header"><span>Order ID: <b>QWE202212</b></span><i class="fa-solid fa-xmark cancel-btn"></i>
 		</div>
 		<div class="card-body">
-			<p class="card-text">
-				<table class="table">
+			<table class="table">
+				<thead>
 					<tr>
 						<th scope="col">#</th>
 						<th scope="col">Item</th>
 						<th scope="col">Price</th>
 					</tr>
+				</thead>
+				<tbody>
 					<tr>
 						<th scope="row">1</th>
 						<td>Medium Corn</td>
@@ -129,11 +83,11 @@ const createOrderCard = (orderId) => {
 						<th colspan="2">Total</th>
 						<td>$17.25</td>
 					</tr>
-				</table>
-			</p>
+				</tbody>
+			</table>
 			<p class="card-text"><span class="badge rounded-pill bg-success" id="QWE202212">Second layer of cheeze
 					added</span></p>
 		</div>
-		<div class="card-footer">August 23, 2022 at 09:50:51 PM</div>
+		<div class="card-footer">August 23, 2022 at 10:37:28 PM</div>
 	</div>
-</div> */
+</div> */}
---
+++ JavaScript/Projects/OrderStatusManagementV2/js/createOrderCardBillTable.js
@@ -0,0 +1,79 @@
+const createOrderCardBillTable = () => {
+	let billDetails = [
+    {
+      item: "Medium Corn",
+      price: 5.00,
+    },
+    {
+      item: "Large Mushroom",
+      price: 8.50,
+    },
+    {
+      item: "Medium Paneer",
+      price: 4.75,
+    },
+  ];
+
+	// Calculate Total Amount 
+	let totalAmount = 0
+	billDetails.forEach(element => {
+		totalAmount = totalAmount + element.price
+	})
+
+	/* ---------------------------------------------------------------- */
+
+	let billTable = document.createElement('table')
+	let billTableHead = document.createElement('thead')
+	let billTableBody = document.createElement('tbody')
+
+	billTable.classList = 'table'
+
+	/* ---------------------------------------------------------------- */
+	// 1. Creating Heading Row
+
+	let billHeadingRow = document.createElement('tr')
+	let billHeadingColumn1 = document.createElement('th')
+	let billHeadingColumn2 = document.createElement('th')
+	let billHeadingColumn3 = document.createElement('th')
+
+	billHeadingColumn1.innerText = '#'
+	billHeadingColumn2.innerText = 'Item'
+	billHeadingColumn3.innerText = 'Price'
+
+	billHeadingRow.append(billHeadingColumn1, billHeadingColumn2, billHeadingColumn3)
+	billTableHead.append(billHeadingRow)
+
+	/* ---------------------------------------------------------------- */
+	// 2. Creating Item Rows
+	let billTableRows = []
+	for(let i = 1; i <= billDetails.length; i++){
+		billTableRows.push(document.createElement('tr'))
+
+		let billTableColumns = []
+		for(let j = 0; j < 3; j++) 
+			billTableColumns.push(document.createElement('td'))
+
+		billTableColumns[0].innerText = i
+		billTableColumns[1].innerText = billDetails[i - 1].item
+		billTableColumns[2].innerText = `$${billDetails[i - 1].price.toFixed(2)}`
+
+		billTableRows[i - 1].append(...billTableColumns)
+	}
+
+	/* ---------------------------------------------------------------- */
+	// 3. Creating Total Bill Row
+	let totalBillTH = document.createElement('th')
+	totalBillTH.setAttribute('colspan', 2)
+	totalBillTH.innerText = 'Total'
+	let totalBillTD = document.createElement('td')
+	totalBillTD.innerText = `$${totalAmount.toFixed(2)}`
+	let totalBillRow = document.createElement('tr')
+	totalBillRow.append(totalBillTH, totalBillTD)
+
+	/* ---------------------------------------------------------------- */
+	billTableBody.append(...billTableRows, totalBillRow)
+	billTable.appendChild(billTableHead)
+	billTable.appendChild(billTableBody)
+
+	return billTable
+}
